4ae7db73322171918c6d0116b49b466fa17fdda1
roushan404 <roushan5395r@gamil.com>
2026-06-05 17:52:06 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,13 @@
+require('dotenv').config()
+const express=require("express")
+const app=express()
+const port=3000;
+app.get('/',(req,res)=>{
+res.send("hello world!")
+})
+app.get('/login',(req,res)=>{
+res.send("my name is roushan")
+})
+app.listen(process.env.PORT,()=>{
+    console.log(`app listening on port ${port}`)
+})
